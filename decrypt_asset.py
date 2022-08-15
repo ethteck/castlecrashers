97dbbd970abc4a311206e6442c013a7b598e18bd
Ethan Roseman <ethteck@gmail.com>
2022-08-16 08:15:05 +0900

    script updates

--- decrypt_asset.py
+++ decrypt_asset.py
@@ -7,7 +7,21 @@ import blowfish
 from ctypes import c_uint32
 from pathlib import Path
 
-def get_cok6_nref(infile: Path):
+keystrings = [
+    b"\x1b\xbf\x18\xcc\x86\x5d\xf4\x25\x07\xc3\xe5\xb3\xb9\x04\x5a\x14\xd7\xfc\x4c\x86\x8d\x4a\xcb\x8f",
+    b"\x24\x53\x4a\x1e\xda\x06\x85\x5f\x7a\xc1\xb6\x8a\x76\x41\x20\xcb\x1f\xce\x61\xd6\xad\x74\x6b\x0f",
+    b"\x77\x82\x1e\x54\x89\xd7\x87\xb6\x05\xf9\x64\xcc\x57\x0b\xcf\x8b\xf8\xd2\x35\x80\x9c\xbf\x9e\x19",
+    b"\x5a\x8d\x84\x20\x6e\x90\xfb\x91\x1f\x48\xe0\xee\xc2\x03\xa2\xaf\x60\x2f\x93\xd6\xa8\x50\x2c\xe2",
+]
+
+key_swap_indexes = [
+    [ 8, 10, 12, 17 ],
+    [ 1, 2, 10, 15 ],
+    [ 0, 9, 12, 16 ],
+    [ 5, 6, 11, 14 ],
+]
+
+def unzip_to_cok6_nref(infile: Path):
     with zipfile.ZipFile(infile, 'r') as f:
         infolist = f.infolist()
         if len(infolist) > 1:
@@ -16,35 +30,23 @@ def get_cok6_nref(infile: Path):
         with f.open(cok6_nref, "r") as fo:
             return cok6_nref.filename.upper(), fo.read()
 
-def decrypt(infile: Path, outdir: Optional[Path] = None):
+def decrypted_to_swf(data: bytes):
     """
-    Decrypts the asset file.
+    Converts the decrypted data to a swf file.
     """
+    out_size = int.from_bytes(data[0x10:0x14], byteorder="little")
+    out_offset = int.from_bytes(data[0x14:0x18], byteorder="little")
 
-    name, input_data = get_cok6_nref(infile)
-    in_size = len(input_data)
+    return data[out_offset:out_size + out_offset]
 
-    keystrings = [
-        b"\x1b\xbf\x18\xcc\x86\x5d\xf4\x25\x07\xc3\xe5\xb3\xb9\x04\x5a\x14\xd7\xfc\x4c\x86\x8d\x4a\xcb\x8f",
-        b"\x24\x53\x4a\x1e\xda\x06\x85\x5f\x7a\xc1\xb6\x8a\x76\x41\x20\xcb\x1f\xce\x61\xd6\xad\x74\x6b\x0f",
-        b"\x77\x82\x1e\x54\x89\xd7\x87\xb6\x05\xf9\x64\xcc\x57\x0b\xcf\x8b\xf8\xd2\x35\x80\x9c\xbf\x9e\x19",
-        b"\x5a\x8d\x84\x20\x6e\x90\xfb\x91\x1f\x48\xe0\xee\xc2\x03\xa2\xaf\x60\x2f\x93\xd6\xa8\x50\x2c\xe2",
-    ]
-
-    key_swap_indexes = [
-        [ 8, 10, 12, 17 ],
-        [ 1, 2, 10, 15 ],
-        [ 0, 9, 12, 16 ],
-        [ 5, 6, 11, 14 ],
-    ]
-
+def blowfish_decrypt(name: str, data: bytes):
     name_crc = c_uint32(0)
     for i in range(len(name)):
         name_crc = c_uint32(name_crc.value * 0x25)
         name_crc = c_uint32(name_crc.value + ord(name[i]))
     
     # Do some math on the size of the .COK6.NREF to get a value from 0 to 15
-    size_tmp = c_uint32(int(in_size / 16) % 16)
+    size_tmp = c_uint32(int(len(data) / 16) % 16)
 
     # Select one of the 4 keystrings (division of the size var by 4)
     key_indexes = key_swap_indexes[int(size_tmp.value / 4)]
@@ -60,17 +62,29 @@ def decrypt(infile: Path, outdir: Optional[Path] = None):
         name_crc = c_uint32(name_crc.value >> 8)
 
     cipher = blowfish.Cipher(bytes(key), byte_order="little")
-    blowfish_decrypted = b"".join(cipher.decrypt_ecb_cts(input_data))
+    return b"".join(cipher.decrypt_ecb(data))
 
-    out_size = int.from_bytes(blowfish_decrypted[0x10:0x14], byteorder="little")
-    out_offset = int.from_bytes(blowfish_decrypted[0x14:0x18], byteorder="little")
+def decrypt_file(infile: Path, outdir: Optional[Path] = None):
+    """
+    Decrypts the asset file.
+    """
+
+    name, input_data = unzip_to_cok6_nref(infile)
+    
+    blowfish_decrypted = blowfish_decrypt(name, input_data)
+
+    swf_bytes = decrypted_to_swf(blowfish_decrypted)
 
     if not outdir:
         outdir = infile.parent
 
     outfile = outdir / (name.split(".")[0] + ".swf")
     with open(outfile, 'wb') as f:
-        f.write(blowfish_decrypted[out_offset : out_size])
+        f.write(swf_bytes)
+
+    outfile_raw = outdir / (name.split(".")[0] + ".swf.raw")
+    with open(outfile_raw, 'wb') as f:
+        f.write(blowfish_decrypted)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -83,4 +97,4 @@ if __name__ == "__main__":
     if args.outdir:
         outdir = Path(args.outdir)
 
-    decrypt(Path(args.infile), outdir)
+    decrypt_file(Path(args.infile), outdir)
